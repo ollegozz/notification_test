8d40ceb40e161e588ffa8c3fd047d965ae47b2e3
Олег <OllegOzz@yandex.ru>
2023-07-13 23:19:37 +0300

    carousel_temp

--- src/componenets/UserPage.jsx
+++ src/componenets/UserPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Carousel from './carousel/Carousel';
 import NotificationModal from './NotificationModal';
 
 export default function UserPage() {
   return (
-    <NotificationModal /> 
+    <>
+      <NotificationModal />
+      <Carousel />
+    </>
+    
   )
 }
---
+++ src/componenets/carousel/Carousel.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './carousel.module.css'
+
+export default function Carousel() {
+  return (
+    <div className={style.body}>Carousel</div>
+  )
+}
